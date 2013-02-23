c3fcbe8e300914c951649a3ef1bb1ecfb9f3d9bc
Erluo Li <el378@cornell.edu>
2013-02-22 20:50:39 -0500

    Add http server to serve tests to client

--- test/cloud.js
+++ test/cloud.js
@@ -2,7 +2,7 @@ require ("./common.js");
 
 var arguments = process.argv.splice(2);
 if (arguments.length != 2){
-  console.log("Need to provide url\nTry: node test.js [id] [test-url]");
+  console.log("Need to provide test id and url\nTry: node test.js [id] [test-url]");
   process.exit(1);
 }
 
@@ -21,4 +21,20 @@ mocha.run(function(){
 })
 
 // start http server
+var http = require('http').createServer(function handler (req, res) {
 
+  var fs = require('fs');
+  console.log(__dirname + "/client_test" + req.url);
+  fs.readFile(__dirname + "/client_test" + req.url,
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading data');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+})
+
+http.listen(8080)
