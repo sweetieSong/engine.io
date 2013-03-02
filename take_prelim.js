0d94abe826110fc1a8e5ca5b0c63180db1d8d296
Sweet Song <shihui.sweet.song@gmail.com>
2013-03-02 00:51:01 -0500

    refactoring only made it loads worse

--- take_prelim.js
+++ take_prelim.js
@@ -1,7 +1,7 @@
-require("test/common.js");
+require("./test/common.js");
 var fs = require('fs');
 
-var files = fs.readdirSync(__dirname + "/tests/cloud_test/prelims/");
+var files = fs.readdirSync(__dirname + "/test/cloud_test/prelims/");
 
 if (files == undefined) {
   console.log('Sorry, error occurred in fetching directory files');
@@ -14,23 +14,25 @@ var Mocha = require('mocha');
 
 var mocha = new Mocha;
 
-mocha.addFile('rwara/cloud_test/Prelim.js');
+mocha.addFile('./test/cloud_test/Prelims.js');
 
 var myArgs = process.argv.slice(2);
 if (myArgs[0] == 'local') {
   for (var i = 0 ; i < files.length ; i ++) {
     global.file = files[i];
     global.url = "http://localhost";
+    global.prelim = require(__dirname + "/test/cloud_test/prelims/" + file);
     mocha.run();
   }
 } else {
   var lt = start_lt();
 
-  lt.on('url', function() {
+  lt.on('url', function (url) {
     console.log("ever get url?");
     for (var i = 0 ; i < files.length ; i ++) {
       global.file = files[i];
       global.url = url;
+      global.prelim = require(__dirname + "/test/cloud_test/prelims/" + file);
       mocha.run();
     }
   });
