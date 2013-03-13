8325132917e3605e9fb903e0cdf0d22aaa88efc1
Sweet Song <shihui.sweet.song@gmail.com>
2013-03-13 02:06:30 -0400

    There's also changes to the node_modules for mocha-cloud and mocha-cloud-grid-view

---
+++ test/cloud_test/cloud.js
@@ -0,0 +1,67 @@
+require('../common.js');
+
+var Canvas = require('term-canvas')
+  , size = process.stdout.getWindowSize()
+  , GridView = require('mocha-cloud-grid-view')
+	, Cloud = require('mocha-cloud')
+  , lt_client = require('localtunnel').client;
+
+
+var cloud = new Cloud("canvas?", global.username, global.userkey);
+cloud.browser('chrome', '', 'Windows 2008');
+cloud.browser('firefox', '17', 'Mac 10.6');
+
+cloud.on('init', function (browser) {
+  console.log('  init : %s %s', browser.browserName, browser.version);
+});
+
+cloud.on('start', function (browser) {
+  console.log('  start : %s %s', browser.browserName, browser.version);
+});
+
+cloud.on('end', function (browser, res) {
+  res.failures.forEach(function(failure){
+//    console.log(failure.fullTitle + "\n");
+  });
+
+});
+
+var client = lt_client.connect({
+  host: 'http://localtunnel.me', 
+  port: 8080
+});
+
+client.on('url', function(url) {
+  console.log(url + '/index.html');
+  cloud.url(url + '/index.html');
+});
+
+// setup
+
+var canvas = new Canvas(size[0], size[1]);
+var ctx = canvas.getContext('2d');
+var grid = new GridView(cloud, ctx);
+grid.size(canvas.width, canvas.height);
+ctx.hideCursor();
+
+// trap SIGINT
+
+process.on('SIGINT', function(){
+  ctx.reset();
+  process.nextTick(function(){
+    process.exit();
+  });
+});
+
+// output failure messages
+// once complete, and exit > 0
+// accordingly
+
+
+cloud.start(function(){
+  grid.showFailures();
+  setTimeout(function(){
+    ctx.showCursor();
+    process.exit(grid.totalFailures());
+  }, 100);
+});
--- test/cloud_test/prelims/test.js
+++
@@ -1,4 +0,0 @@
-var Mocha = require('mocha');
-var mocha = new Mocha;
-mocha.addFile('json.js');
-mocha.run();
